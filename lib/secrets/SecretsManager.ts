e90b88086c9bd0df1cf3fa087b02735b51ca65d7
whennemuth <wrh@bu.edu>
2025-12-29 14:58:59 -0500

    Adding parameters for createOrUpdateSecrets to override values derived from context.json

--- lib/secrets/SecretsManager.ts
+++ lib/secrets/SecretsManager.ts
@@ -13,10 +13,13 @@ import { CreateSecretCommandOutput, UpdateSecretCommandOutput } from "@aws-sdk/c
 export const createOrUpdateSecrets = async (): Promise<string> => {
   const context = contextJSON as IContext;
 
-  const { STACK_ID, ORIGIN: { stackId: ORIGIN_STACK_ID } = {}, REGION, TAGS: { Landscape }, SHIBBOLETH: { entityId, idpCert, secret: { 
+  const { STACK_ID, ORIGIN: { stackId: ORIGIN_STACK_ID } = {}, REGION, SHIBBOLETH: { entityId, idpCert, secret: { 
     jwtPrivateKeySecretFld, jwtPublicKeySecretFld, 
     samlCertSecretFld, samlPrivateKeySecretFld
   } } } = context;
+
+  // Landscape can be overridden by env variable LANDSCAPE.
+  let { TAGS: { Landscape } } = context;
 
   const fldNames = {
     jwtPrivateKeySecretFld,
@@ -25,14 +28,7 @@ export const createOrUpdateSecrets = async (): Promise<string> => {
     samlPrivateKeySecretFld
   };
 
-  const secretName = ORIGIN_STACK_ID ? 
-    `${STACK_ID}/${ORIGIN_STACK_ID}/${Landscape}` : 
-    `${STACK_ID}/${Landscape}`;
-
-  const description = `Stores shib key and cert pem content for ${entityId}`;
-  const smSecret = new SecretsManagerSecret({ secretName, description, fldNames, region: REGION });
-
-  const { SAML_CERT, SAML_PK, CLOUDFRONT_CHALLENGE } = process.env;
+  const { SAML_CERT, SAML_PK, CLOUDFRONT_CHALLENGE, LANDSCAPE } = process.env;
 
   if( ! CLOUDFRONT_CHALLENGE ) {
     throw new Error('CLOUDFRONT_CHALLENGE environment variable must be set');
@@ -41,6 +37,27 @@ export const createOrUpdateSecrets = async (): Promise<string> => {
   if( ! SAML_PK ) {
     throw new Error('SAML_PK environment variable must be set');
   }
+
+  const args = process.argv.slice(1);
+  let secretNameOverride = '';
+  args.forEach((arg) => {
+    const [key, value] = arg.split('=');
+    if( key.trim().toUpperCase() === 'LANDSCAPE' ) {
+      console.log(`Overriding Landscape tag from ${Landscape} from context to ${value} from argument`);
+      Landscape = value;
+    }
+    if( key.trim().toUpperCase() === 'SECRET_NAME' ) {
+      console.log(`Overriding entire secret name to ${value} from argument`);
+      secretNameOverride = value;
+    }
+  });
+
+  const secretName = secretNameOverride || ( ORIGIN_STACK_ID ? 
+    `${STACK_ID}/${ORIGIN_STACK_ID}/${Landscape}` : 
+    `${STACK_ID}/${Landscape}`);
+
+  const description = `Stores shib key and cert pem content for ${entityId}`;
+  const smSecret = new SecretsManagerSecret({ secretName, description, fldNames, region: REGION });
 
   smSecret
     .setCloudFrontChallenge(CLOUDFRONT_CHALLENGE)
