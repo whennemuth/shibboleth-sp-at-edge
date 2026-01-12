64bb13d940e6bfabdfb1a026c3951cd22f480c54
whennemuth <wrh@bu.edu>
2026-01-12 15:49:37 -0500

    Changing build output so it all goes into a dist folder

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,18 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  roots: ['<rootDir>'],
+  roots: ['<rootDir>/lib', '<rootDir>/bin'],
   testMatch: ['**/*.test.ts', '**/*.test.js'],
+  testPathIgnorePatterns: [
+    '<rootDir>/dist/',
+    '<rootDir>/node_modules/'
+  ],
   transform: {
     '^.+\\.tsx?$': 'ts-jest'
   },
   collectCoverageFrom: [
     '**/*.{ts,tsx}',
-    '!**/*.d.ts'
+    '!**/*.d.ts',
+    '!**/dist/**'
   ]
 };
